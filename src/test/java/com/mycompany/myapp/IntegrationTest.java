4a7ec0cac6c73dcb5f91d1b819d43c4c07a2e395
aris-xurpas <aris.reguyal@xurpas.com>
2022-03-10 10:58:51 +0800

    Initial version of blog2nd generated by generator-jhipster@7.7.0

---
+++ src/test/java/com/mycompany/myapp/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.mycompany.myapp;
+
+import com.mycompany.myapp.Blog2ndApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = Blog2ndApp.class)
+public @interface IntegrationTest {
+}
